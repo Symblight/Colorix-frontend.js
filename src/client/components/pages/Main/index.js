542791753497ad3bb0299a559381f8078755c472
Aleksey <symblight@gmail.com>
2018-09-09 12:54:17 +0300

    ✏️ add links

--- src/client/components/pages/Main/index.js
+++ src/client/components/pages/Main/index.js
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 
-export const MainPage = () => <h2>MainPage</h2>
+export class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Main page</h2>
+        <Link to="/palettes">Palettes</Link>
+      </div>
+    )
+  }
+}
--- src/client/components/pages/Palettes/index.js
+++ src/client/components/pages/Palettes/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 
 export class PalettesPage extends Component {
@@ -6,6 +7,7 @@ export class PalettesPage extends Component {
     return (
       <div>
         <h2>PalettesPage</h2>
+        <Link to="/">Home</Link>
       </div>
     )
   }
