18e828784cbaec8ae9d9f3748e6d07bbc08ec833
Aleksey <symblight@gmail.com>
2018-09-28 16:55:37 +0300

    fix

--- src/ui/atoms/Paragraph/index.story.js
+++ src/ui/atoms/Paragraph/index.story.js
@@ -7,7 +7,7 @@ import { Paragraph } from '.'
 storiesOf('ui/Atoms/Paragraph', module)
   .addWithJSX('default', () => (
     <Paragraph>
-        Hi! Just finished the interior of Zooey's and Gus' house for #santria and I will start with some preparation based on the 
+        Hi! Just finished the interior of Zooey's and Gus' house for #santria and I will start with some preparation based on the
         poll for next week! Please enjoy my fondly
-        </Paragraph>
+    </Paragraph>
   ))
