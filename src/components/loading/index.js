5ef2ef724fe3bea807c3d412df75c6220386828f
Aleksey <symblight@gmail.com>
2018-10-01 17:59:41 +0300

    lodable

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,4 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { Icon } from 'ui'
 
 
-export const Loading = () => (<h2>Loading...</h2>)
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+`
+
+
+export class Loading extends React.Component {
+  render() {
+    const { pastDelay } = this.props
+
+    if (pastDelay) {
+      return (
+        <Wrapper>
+          <Icon height={80} icon='loading' />
+          <h3>Loading...</h3>
+        </Wrapper>
+
+      )
+    }
+    return <div>lol</div>
+  }
+}
